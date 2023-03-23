fe35792e6a917193c4b48d448b55568b390116a8
-Brownlolaoo <-aramyeedey199@yahoo.com>
2023-03-23 14:28:39 +0800

    alx-react

--- 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -2,15 +2,15 @@ import * as notificationData from "../../../../notifications.json";
 import { normalize, schema } from "normalizr";
 
 const user = new schema.Entity("users");
-const message = new schema.Entity("message", {}, { idAttribute: "guid" });
-const notifications = new schema.Entity("notification", {
+const message = new schema.Entity("messages", {}, { idAttribute: "guid" });
+const notification = new schema.Entity("notification", {
   author: user,
   context: message,
 });
 
 const normalized = normalize(notificationData, [notification]);
 
-export default function getAllNotificationsBuyUser(userId) {
+export default function getAllNotificationsByUser(userId) {
   return notificationData.filter((notification) => notification.author.id === userId).map((notification) => notification.context);
 }
 
